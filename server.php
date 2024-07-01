fa767b8d276d0d015e3aa759e7092e30fad8c9b1
raghveerdeol <deolraghveer5@gmail.com>
2024-07-01 17:34:51 +0200

    decodificati i dati json in php

--- server.php
+++ server.php
@@ -1,4 +1,6 @@
 <?php 
-
-header("content-type: application/json");
+// header("content-type: application/json");
+$rawListaTodO = file_get_contents("./db/todoList.json");
+$listaTodo = json_decode($rawListaTodO, true);
+var_dump($listaTodo)
 ?>
